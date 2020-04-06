1f8c6f180c03fba9f0a104849de8c343eb8c3501
Maina George <nuriszka@gmail.com>
2020-04-06 16:30:42 +0200

    Added Details Page for Service Types

---
+++ Pages/ServiceTypes/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SparkAuto.Data.ApplicationDbContext _db;
+
+        public DetailsModel(SparkAuto.Data.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? serviceId)
+        {
+            if (serviceId == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == serviceId);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
