e7d4135e7abf87199384c5c68a52e7b6b01d658a
Maina George <nuriszka@gmail.com>
2020-04-07 12:51:11 +0200

    Added a custom TagHelper for user pagination
    The list of users is now going to be paginated using this custom helper

---
+++ Models/PaginationDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SparkAuto.Models
+{
+    public class PaginationDetails
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int) Math.Ceiling((decimal) TotalItems / ItemsPerPage);
+
+        public string Url { get; set; } 
+    }
+}
---
+++ TagHelpers/PaginationTagHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SparkAuto.Models;
+
+namespace SparkAuto.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PaginationTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _helperFactory;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }    //provide access to requests and responses
+
+        public PaginationDetails PaginationDetails { get; set; }
+
+        public string PageAction { get; set; }
+        public string PageCss { get; set; }
+        public string UnSelectedPagesCss { get; set; }
+        public string SelectedPagesCss { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            var divTagBuilder = new TagBuilder("div");
+
+            for (var i = 1; i <= PaginationDetails.TotalPages; i++)
+            {
+                var aTag = new TagBuilder("a");
+                var url = PaginationDetails.Url.Replace(":", i.ToString());
+                aTag.Attributes["href"] = url;
+                aTag.AddCssClass(PageCss);
+
+                aTag.AddCssClass(i == PaginationDetails.CurrentPage ? SelectedPagesCss : UnSelectedPagesCss);
+                aTag.InnerHtml.Append(i.ToString());
+
+                divTagBuilder.InnerHtml.AppendHtml(aTag);
+            }
+
+            output.Content.AppendHtml(divTagBuilder.InnerHtml);
+        }
+
+        public PaginationTagHelper(IUrlHelperFactory helperFactory)
+        {
+            _helperFactory = helperFactory;
+        }
+    }
+}
