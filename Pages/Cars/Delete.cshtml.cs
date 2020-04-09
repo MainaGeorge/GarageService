8ab7f1e00d30c46f458101838126c2f0609cce8e
Maina George <nuriszka@gmail.com>
2020-04-09 10:42:57 +0200

    Added deletion of cars from a user

---
+++ Pages/Cars/Delete.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Cars
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Car Car { get; set; }
+        public async Task<IActionResult> OnGet(int? carId)
+        {
+            if (!carId.HasValue)
+            {
+                return NotFound("There is no such car");
+            }
+
+            Car = await _db.Car.FirstOrDefaultAsync(c => c.Id == carId);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var dbCar = await _db.Car.FirstOrDefaultAsync(c => c.Id == Car.Id);
+
+            if (dbCar == null)
+            {
+                return NotFound("NO such car found in the database");
+            }
+
+            _db.Car.Remove(dbCar);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index", new
+            {
+                userId = Car.UserId
+            });
+        }
+    }
+}
