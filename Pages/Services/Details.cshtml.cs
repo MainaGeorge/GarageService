cd375295507c1f51a6b25fd004a1323d87e92c25
Maina George <nuriszka@gmail.com>
2020-04-12 20:31:57 +0200

    Added the Details of services performed on a given date

---
+++ Pages/Services/Details.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Services
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ServiceDetails ServiceDetails { get; set; }
+        public ServiceHeader ServiceHeader { get; set; }
+        public List<ServiceDetails> ServiceDetailsList { get; set; }
+
+        public void OnGet(int serviceHeaderId)
+        {
+            ServiceHeader = _db.ServiceHeader.Include(c => c.Car)
+                .Include(c => c.Car.ApplicationUser)
+                .FirstOrDefault(s => s.Id == serviceHeaderId);
+
+            ServiceDetailsList = _db.ServiceDetails.Where(c => c.ServiceHeaderId == serviceHeaderId).ToList();
+            ServiceDetails = ServiceDetailsList.FirstOrDefault();
+        }
+    }
+}
