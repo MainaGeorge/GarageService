dbf038c234e292aee43bbd8057d52ec9fd7cdcb8
MainaGeorge <nuriszka@gmail.com>
2020-05-05 20:01:37 +0200

    Added the shopping cart  onget view

---
+++ Models/ViewModel/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Models.ViewModel
+{
+    public class ShoppingCartViewModel
+    {
+        public List<ServiceHeader> ServiceHeaders { get; set; }
+        public Dictionary<int, List<ServiceDetails>> ServiceDetails { get; set; }
+
+        public double TotalPrice { get; set; }
+    }
+}
--- Pages/ShoppingCart/ShoppingCart.cshtml.cs
+++ Pages/ShoppingCart/ShoppingCart.cshtml.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+using SparkAuto.Models.ViewModel;
 
 namespace SparkAuto.Pages.ShoppingCart
 {
     public class ShoppingCartModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _db;
 
+        public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
+
+        public ShoppingCartModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userId = claim.Value;
+
+            var unpaidServices = new List<ServiceHeader>();
+            var cars = await _db.Car.Where(c => c.UserId == userId).Select(c => c.Id).ToListAsync();
+
+            foreach (var car in cars)
+            {
+                var serviceHeaders = await _db.ServiceHeader.Where(c => c.CarId == car && !c.IsPaid)
+                    .ToListAsync();
+                unpaidServices.AddRange(serviceHeaders);
+            }
+
+            ShoppingCartViewModel = new ShoppingCartViewModel()
+            {
+                ServiceHeaders = unpaidServices,
+                ServiceDetails = new Dictionary<int, List<ServiceDetails>>()
+            };
+
+            foreach (var serviceHeader in ShoppingCartViewModel.ServiceHeaders)
+            {
+                var details = await _db.ServiceDetails.Where(s => s.ServiceHeaderId == serviceHeader.Id).ToListAsync();
+                ShoppingCartViewModel.ServiceDetails[serviceHeader.Id] = details;
+            }
+
+            ShoppingCartViewModel.TotalPrice = ShoppingCartViewModel.ServiceHeaders.Sum(c => c.TotalPrice);
+
+            return Page();
         }
     }
 }
